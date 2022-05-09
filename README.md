0860e80a58180dbe3938346a27ae75d59e10cb0d
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-09 11:18:57 +0200

    Add negative synonyms curation

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A decentralized database of synonyms for biomedical entities and concepts.
 
+### Synonyms
+
 The [`synonyms.tsv`](synonyms.tsv) has four columns:
 
 1. `curie` the compact uniform resource identifier (CURIE) for a biomedical
@@ -21,3 +23,23 @@ Here's an example of some rows in the synonyms table (with linkified CURIEs):
 |---------------------------------------------------|---------------------------------|-----------------------------------------------------------|----------------------------------------------------------------------------------------------------------------------|
 | [CHEBI:16618](https://bioregistry.io/CHEBI:16618) | PI(3,4,5)P3                     | [skos:exactMatch](https://bioregistry.io/skos:exactMatch) | [pubmed:29623928](https://bioregistry.io/pubmed:29623928), [pubmed:20817957](https://bioregistry.io/pubmed:20817957) |
 | [CHEBI:16618](https://bioregistry.io/CHEBI:16618) | phosphatidylinositol (3,4,5) P3 | [skos:exactMatch](https://bioregistry.io/skos:exactMatch) | [pubmed:29695532](https://bioregistry.io/pubmed:29695532)                                                            |
+
+### Incorrect Synonyms
+
+The [`negatives.tsv`](negatives.tsv) has three columns for non-trivial examples
+of text strings that aren't synonyms. This document doesn't address the same
+issues as context-based disambiguation, but rather helps dscribe issues like
+incorrect sub-string matching:
+
+1. `curie` the compact uniform resource identifier (CURIE) for a biomedical
+   entity or concept that **does not** match the following text, standardized
+   using the Bioregistry
+2. `text` the non-synonym text itself
+3. `references` same as for `synonyms.tsv`, illustrating documents where this
+   string appears
+
+Here's an example of some rows in the negative synonyms table:
+
+| curie                                           | text        | references                                                                                                           |
+|-------------------------------------------------|-------------|----------------------------------------------------------------------------------------------------------------------|
+| [hgnc:22979](https://bioregistry.io/hgnc:22979) | PI(3,4,5)P3 | [pubmed:29623928](https://bioregistry.io/pubmed:29623928), [pubmed:20817957](https://bioregistry.io/pubmed:20817957) | 
--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -8,7 +8,8 @@ import bioregistry
 
 HERE = Path(__file__).parent.resolve()
 ROOT = HERE.parent.resolve()
-PATH = ROOT.joinpath("synonyms.tsv")
+POSITIVES_PATH = ROOT.joinpath("synonyms.tsv")
+NEGATIVES_PATH = ROOT.joinpath("negatives.tsv")
 
 SYNONYM_TYPES = {
     "skos:exactMatch",
@@ -19,11 +20,6 @@ SYNONYM_TYPES = {
 
 class TestIntegrity(unittest.TestCase):
     """Test case for data integrity tests."""
-
-    def setUp(self):
-        """Set up the test case by loading the synonyms file."""
-        with PATH.open() as file:
-            self.header, *self.rows = [tuple(line.strip().split("\t")) for line in file]
 
     def assert_curie(self, curie: str):
         """Assert a CURIE is standardized against the Bioregistry.
@@ -43,9 +39,12 @@ class TestIntegrity(unittest.TestCase):
         if pattern:
             self.assertRegex(identifier, pattern)
 
-    def test_rows(self):
-        """Test each row."""
-        for row_index, row in enumerate(self.rows, start=1):
+    def test_positives(self):
+        """Test each row in the positives file."""
+        with POSITIVES_PATH.open() as file:
+            _, rows = [tuple(line.strip().split("\t")) for line in file]
+
+        for row_index, row in enumerate(rows, start=1):
             with self.subTest(row=row_index):
                 self.assertEquals(4, len(row))
                 curie, text, stype, references = row
@@ -57,9 +56,32 @@ class TestIntegrity(unittest.TestCase):
                     self.assert_curie(reference)
 
         # test sorted
-        self.assertEqual(sorted(self.rows), self.rows, msg="synonyms are not properly sorted")
+        self.assertEqual(sorted(rows), rows, msg="synonyms are not properly sorted")
 
         # test no duplicates
-        c = Counter(row[:2] for row in self.rows)
+        c = Counter(row[:2] for row in rows)
+        duplicated = {key: count for key, count in c.items() if count > 1}
+        self.assertEqual(0, len(duplicated), msg=f"duplicated entries: {duplicated}")
+
+    def test_negatives(self):
+        """Test each row in the negatives file."""
+        with NEGATIVES_PATH.open() as file:
+            _, *rows = [tuple(line.strip().split("\t")) for line in file]
+
+        for row_index, row in enumerate(rows, start=1):
+            with self.subTest(row=row_index):
+                self.assertEquals(3, len(row))
+                curie, text, references = row
+                self.assertLess(1, len(text), msg="can not have 1 letter synonyms")
+                self.assert_curie(curie)
+                for reference in references.split(","):
+                    reference = reference.strip()
+                    self.assert_curie(reference)
+
+        # test sorted
+        self.assertEqual(sorted(rows), rows, msg="negative synonyms are not properly sorted")
+
+        # test no duplicates
+        c = Counter(row[:2] for row in rows)
         duplicated = {key: count for key, count in c.items() if count > 1}
         self.assertEqual(0, len(duplicated), msg=f"duplicated entries: {duplicated}")
