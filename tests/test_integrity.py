90dd075e52a23786b61c1b2daa7e2fa5d60359e2
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-09 10:43:44 +0200

    Add tests for duplicates and order

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -1,6 +1,7 @@
 """Tests for data integrity of biosynoynms database."""
 
 import unittest
+from collections import Counter
 from pathlib import Path
 
 import bioregistry
@@ -22,7 +23,7 @@ class TestIntegrity(unittest.TestCase):
     def setUp(self):
         """Set up the test case by loading the synonyms file."""
         with PATH.open() as file:
-            self.header, *self.rows = [line.strip().split("\t") for line in file]
+            self.header, *self.rows = [tuple(line.strip().split("\t")) for line in file]
 
     def assert_curie(self, curie: str):
         """Assert a CURIE is standardized against the Bioregistry.
@@ -44,8 +45,6 @@ class TestIntegrity(unittest.TestCase):
 
     def test_rows(self):
         """Test each row."""
-        # TODO check duplicates
-        # TODO check canonical sort order
         for row_index, row in enumerate(self.rows, start=1):
             with self.subTest(row=row_index):
                 self.assertEquals(4, len(row))
@@ -56,3 +55,11 @@ class TestIntegrity(unittest.TestCase):
                 for reference in references.split(","):
                     reference = reference.strip()
                     self.assert_curie(reference)
+
+        # test sorted
+        self.assertEqual(sorted(self.rows), self.rows, msg="synonyms are not properly sorted")
+
+        # test no duplicates
+        c = Counter(row[:2] for row in self.rows)
+        duplicated = {key: count for key, count in c.items() if count > 1}
+        self.assertEqual(0, len(duplicated), msg=f"duplicated entries: {duplicated}")
