64d43ed6ca9e23e2fa3c29c6f3d1c9058f13b4d6
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-12-13 13:26:27 +0100

    Update __init__.py

--- src/biosynonyms/resources/__init__.py
+++ src/biosynonyms/resources/__init__.py
@@ -15,7 +15,7 @@ from typing import (
 
 import pandas as pd
 import requests
-from curies import Reference, NamedReference
+from curies import NamedReference, Reference
 from pydantic import BaseModel, Field
 from pydantic_extra_types.language_code import LanguageAlpha2
 from tqdm import tqdm
